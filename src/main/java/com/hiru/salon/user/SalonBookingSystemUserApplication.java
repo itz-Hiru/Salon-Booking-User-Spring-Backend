aeb0e69c1c4dbd261ee58ff9aaa7bd9044c91f7c
itz-Hiru <hirumithagithub@gmail.com>
2025-04-06 10:51:22 +0530

    Created simple spring project

---
+++ src/main/java/com/hiru/salon/user/SalonBookingSystemUserApplication.java
@@ -0,0 +1,13 @@
+package com.hiru.salon.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonBookingSystemUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonBookingSystemUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiru/salon/user/SalonBookingSystemUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiru.salon.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonBookingSystemUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
